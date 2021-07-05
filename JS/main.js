6178b048028fc18b17860116204cf3f891f40987
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2021-07-05 19:31:59 +0100

    Finished

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+function get_messages(){
+    return new Promise((resolve, reject) =>{
+        url = 'https://ajayvhd.pythonanywhere.com/mes'
+        fetch(url).then(response => response.json()).then(data => resolve(data));
+    })
+}
+
+get_messages().then(data => create_items(data))
+
+function create_items(data){
+    console.log(data[1])
+    const texts = document.getElementById("texts");
+    for (let i = 0; i < data.length; i++) 
+    {
+        const Div = document.createElement("div");
+        Div.classList = "mes"
+        
+        const p1 = document.createElement("p");
+        const Message = document.createTextNode(data[i]["message"]);
+        p1.appendChild(Message)
+        p1.classList = "motd"
+
+        formattedTime = timeConverter(data[i]["sent_timestamp"])
+        const p2 = document.createElement("p");
+        const SenderInfo = document.createTextNode(formattedTime + " " + data[i]["from"]);
+        p2.appendChild(SenderInfo)
+        p2.classList = "info"
+
+        Div.appendChild(p1)
+        Div.appendChild(p2)
+
+        texts.appendChild(Div)
+
+    }
+}
+
+
+function timeConverter(UNIX_timestamp){
+    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+    d.setUTCSeconds(UNIX_timestamp/1000)
+    return d.toLocaleString()
+  }
